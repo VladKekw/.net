fb6a41f781ef6b5d9738374f065af960e26447c2
Vladyslav Sorokolit <wwlddss@gmail.com>
2024-11-28 12:14:55 +0200

    lab 234 done

--- VladKekw.TaskPlanner.Domain.Logic/VladKekw.TaskPlanner.Domain.Logic/obj/Release/net6.0/VladKekw.TaskPlanner.Domain.Logic.AssemblyInfo.cs
+++ VladKekw.TaskPlanner.Domain.Logic/VladKekw.TaskPlanner.Domain.Logic/obj/Release/net6.0/VladKekw.TaskPlanner.Domain.Logic.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("VladKekw.TaskPlanner.Domain.Logic")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d44a574f6a39a640f338fedf10b8f21e8e39fb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("VladKekw.TaskPlanner.Domain.Logic")]
--- VladKekw.TaskPlanner.Domain.Models/VladKekw.TaskPlanner.Domain.Models/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace VladKekw.TaskPlanner.Domain.Models
-{
-    public class Class1
-    {
-
-    }
-}
